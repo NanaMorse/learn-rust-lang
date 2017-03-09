8aa4803fc631621a7cb4750864fcd005e25a3fbe
Morse Guo <morse.guo@dji.com>
2017-03-09 21:54:25 +0800

    test variable bindings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello world!");
+    // variable-bindings
+    let my_number = 0;
+    let my_string = "hello string";
+
+    let (first_var, second_var) = ("first_var", 2);
+
+    let mut mutable_var = 1;
+    mutable_var = 2;
+
+    let integer_32_bit: i32 = 65537;
+
+    print!("{}", integer_32_bit);
 }
