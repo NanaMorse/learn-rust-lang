6b496c4f918bb6bf129eef488d27611072a299f9
Morse Guo <morse.guo@dji.com>
2017-03-21 12:11:38 +0800

    4.2: Functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
 fn main() {
     // variable-bindings
-    let my_number = 0;
-    let my_string = "hello string";
+    {
+        let my_number = 0;
+        let my_string = "hello string";
 
-    let (first_var, second_var) = ("first_var", 2);
+        // Patterns
+        let (first_var, second_var) = ("first_var", 2);
 
-    let mut mutable_var = 1;
-    mutable_var = 2;
+        // Type annotations
+        let integer_32_bit: i32 = 15;
 
-    let integer_32_bit: i32 = 65537;
+        // Mutability
+        let mut mutable_var = 1;
+        mutable_var = 2;
 
-    print!("{}", integer_32_bit);
+        print!("{}", integer_32_bit);
+    }
+
+    // functions
+    {
+        // print number
+        fn print_number(x: i32) {
+            print!("x is: {}", x);
+        }
+
+        // get number
+        fn plus_one(x: i32) -> i32 {
+            x + 1
+        }
+
+        // bad practice
+        fn use_return() -> i32 {
+            return 12;
+        }
+
+        // Diverging functions
+        fn diverges() {
+            panic!("This Function Never returns");
+        }
+
+        // function pointer
+        let f: fn(i32) -> i32;
+        f = plus_one;
+
+        let seven = f(6);
+    }
 }
